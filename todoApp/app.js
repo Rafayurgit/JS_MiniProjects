0769eaa58acf746596e3b55a1488d1b5bd481235
farruop <rafayKhanpvt@gmail.com>
2025-02-12 02:13:39 +0530

    Add: created basic functionality for todo app using js

---
+++ todoApp/app.js
@@ -0,0 +1,32 @@
+const ul = document.querySelector("ul");
+const task = document.querySelector(".taskInput")
+const btn= document.querySelector(".addbtn")
+
+
+btn.addEventListener("click", ()=>{
+    console.log("btn clicked");
+    const li= document.createElement("li");
+    li.innerText=task.value;
+
+    const deleteBtn= document.createElement("button");
+    deleteBtn.innerText="delete"
+    deleteBtn.classList.add("delete");
+
+    li.appendChild(deleteBtn)
+    ul.appendChild(li);
+    task.value="";
+
+
+    let delBtns = document.querySelectorAll(".delete");
+
+for(delBtn of delBtns){
+    delBtn.addEventListener("click", function(){
+        let par= this.parentElement;
+        par.remove();   
+        
+    })        
+}
+})
+
+
+
